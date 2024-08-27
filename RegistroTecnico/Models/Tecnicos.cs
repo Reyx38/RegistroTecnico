592547d0a90c747b21d769ae0f89bc960c600a9c
Reyphill Nuñez <reyphillduarte06@gmail.com>
2024-08-27 14:08:12 -0400

    Cambios: agregamos las carpetas DAL y Models Dentro de DAL se agrego el archivo .cs Contexto Dentro de Models se agrego el archivo .cs Tecnicos se agrego la configuracion ConnectionString al .Json Se inyecto el contexto al Program.cs

---
+++ RegistroTecnico/Models/Tecnicos.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnico.Models
+{
+	public class Tecnicos
+	{
+		[Key]
+		[Required(ErrorMessage = "Campo de nombre obligatorio")]
+		public string? nombre { get; set; }
+		[Required(ErrorMessage = "Campo de apellido obligatorio")]
+		public string? Apellido { get; set; }
+		public int tecniCold { get; set; }
+		public double sueldoHora { get; set; }
+	}
+}
--- RegistroTecnico/Program.cs
+++ RegistroTecnico/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using RegistroTecnico.DAL;
 using RegistroTecnico.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +17,11 @@ if (!app.Environment.IsDevelopment())
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
 }
+
+//obtenemos el ContStr
+var ConStr = builder.Configuration.GetConnectionString("ConStr");
+//Agregamos el contexto
+builder.Services.AddDbContext<Contexto>(options => options.UseSqlite(ConStr));
 
 app.UseHttpsRedirection();
 
