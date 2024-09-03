fe3ab3cd90ac02b2e3005eec3434b49994186a8f
Reyphill Nuñez <reyphillduarte06@gmail.com>
2024-09-03 15:16:31 -0400

    Se ha cambiado el constructor de la clase a primary constructor. #11

---
+++ RegistroTecnicos/DAL/Contexto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTecnicos.Models;
+
+namespace RegistroTecnicos.DAL
+{
+	public class Contexto(DbContextOptions<Contexto> options) : DbContext(options)
+	{
+        public DbSet<Tecnicos> Tecnicos { get; set; }
+	}
+}
