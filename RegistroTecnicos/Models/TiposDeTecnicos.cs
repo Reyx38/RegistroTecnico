ca57b1519fa70bf2adff9c830e814e6e3c13f513
Reyphill Nuñez <reyphillduarte06@gmail.com>
2024-09-04 00:44:31 -0400

    se ha creado el modelo TipoDeTecnicos en la carpeta models

---
+++ RegistroTecnicos/Models/TiposDeTecnicos.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class TiposDeTecnicos
+{
+	[Key]
+	public int TipoDeTecnicosId { get; set; }
+
+    [Required(ErrorMessage = "Completar este campo")]
+    public String? Descripcion { get; set; }
+
+}
