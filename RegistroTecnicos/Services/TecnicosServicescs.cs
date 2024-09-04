15d519eeae2f10949ae8eaa21d361c8519157a44
Reyphill Nuñez <reyphillduarte06@gmail.com>
2024-09-03 22:48:07 -0400

    La variable _Contexto fue cambiada a su forma camel case. #6

--- RegistroTecnicos/Services/TecnicosServicescs.cs
+++ RegistroTecnicos/Services/TecnicosServicescs.cs
@@ -7,36 +7,36 @@ namespace RegistroTecnicos.Services;
 
 public class TecnicosServices(Contexto Contexto)
 {
-    public readonly Contexto _Contexto = Contexto;
+    public readonly Contexto _contexto = Contexto;
 
     public async Task<bool> ExisteNombre(String nombre)
     {
-        return await _Contexto.Tecnicos
+        return await _contexto.Tecnicos
             .AnyAsync(T => T.nombre == nombre);
     }
     public async Task<bool> ExisteId(int id)
     {
-        return await _Contexto.Tecnicos.AnyAsync(t => t.tecniCold == id);
+        return await _contexto.Tecnicos.AnyAsync(t => t.tecniCold == id);
     }
     private async Task<bool> Insertar(Tecnicos tecnicos)
     {
-        await _Contexto.Tecnicos.AddAsync(tecnicos);
-        return await _Contexto.SaveChangesAsync() > 0;
+        await _contexto.Tecnicos.AddAsync(tecnicos);
+        return await _contexto.SaveChangesAsync() > 0;
     }
 
     private async Task<bool> Modificar(Tecnicos tecnicos)
     {
         try
         {
-            var entidadExistente = await _Contexto.Tecnicos.FindAsync(tecnicos.tecniCold);
+            var entidadExistente = await _contexto.Tecnicos.FindAsync(tecnicos.tecniCold);
 
             if (entidadExistente == null)
             {
                 throw new InvalidOperationException("El técnico no existe en la base de datos.");
             }
 
-            _Contexto.Entry(entidadExistente).CurrentValues.SetValues(tecnicos);
-            return await _Contexto.SaveChangesAsync() > 0;
+            _contexto.Entry(entidadExistente).CurrentValues.SetValues(tecnicos);
+            return await _contexto.SaveChangesAsync() > 0;
         }
         catch (InvalidOperationException ex)
         {
@@ -55,21 +55,21 @@ public class TecnicosServices(Contexto Contexto)
 
     public async Task<bool> Eliminar(int tecniCold)
     {
-        return await _Contexto.Tecnicos.
+        return await _contexto.Tecnicos.
             Where(p => p.tecniCold == tecniCold).ExecuteDeleteAsync() > 0;
 
     }
 
     public async Task<Tecnicos?> Buscar(int tecnicold)
     {
-        return await _Contexto.Tecnicos.
+        return await _contexto.Tecnicos.
             AsNoTracking()
             .FirstOrDefaultAsync(p => p.tecniCold == tecnicold);
     }
 
     public async Task<List<Tecnicos>> Listar(Expression<Func<Tecnicos, bool>> criterio)
     {
-        return await _Contexto.Tecnicos
+        return await _contexto.Tecnicos
             .AsNoTracking()
             .Where(criterio)
             .ToListAsync();
