be4ac7b8c6f9be86ea9f4ae457ba6d92ee3a2dd5
Reyphill Nuñez <reyphillduarte06@gmail.com>
2024-09-10 14:42:25 -0400

    update on the model Trabajo

---
+++ RegistroTecnicos/Models/Trabajos.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroTecnicos.Models;
+
+public class Trabajos
+{
+	[Key]
+	public int TrabajoId { get; set; }
+
+	[StringLength(250)]	
+	public string? Descripcion { get;set; }
+
+	public DateTime? Fecha { get; set; }
+	[Required (ErrorMessage ="Campo Obligatorio")]
+	public double Monto { get; set; }
+
+	[ForeignKey("TecnicoId")]
+	public int TecnicoId { get; set; }
+	public Tecnicos? Tecnicos { get; set; } 
+
+
+}
