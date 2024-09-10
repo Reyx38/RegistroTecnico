620325fef5d4bb2d3de9219d84d495cd4fab9751
Reyphill Nuñez <reyphillduarte06@gmail.com>
2024-09-10 13:46:30 -0400

    add Clientes on the context

--- RegistroTecnicos/DAL/Contexto.cs
+++ RegistroTecnicos/DAL/Contexto.cs
@@ -5,6 +5,12 @@ namespace RegistroTecnicos.DAL;
 
 public class Contexto(DbContextOptions<Contexto> options) : DbContext(options)
 {
-    public DbSet<Tecnicos> Tecnicos { get; set; }
-    public DbSet<TiposTecnicos> TiposTecnicos { get; set; }
+	public DbSet<Tecnicos> Tecnicos { get; set; }
+	public DbSet<TiposTecnicos> TiposTecnicos { get; set; }
+	public DbSet<Clientes> Clientes { get; set; }
 }
+
+	
+
+	
+
