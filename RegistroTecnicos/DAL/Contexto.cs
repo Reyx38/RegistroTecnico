ceaaccd7b87caa11a442c3ae4a062274da8c4580
Reyphill Nuñez <reyphillduarte06@gmail.com>
2024-09-10 23:06:14 -0400

    add Trabajos to Dbset

--- RegistroTecnicos/DAL/Contexto.cs
+++ RegistroTecnicos/DAL/Contexto.cs
@@ -8,6 +8,7 @@ public class Contexto(DbContextOptions<Contexto> options) : DbContext(options)
 	public DbSet<Tecnicos> Tecnicos { get; set; }
 	public DbSet<TiposTecnicos> TiposTecnicos { get; set; }
 	public DbSet<Clientes> Clientes { get; set; }
+	public DbSet<Trabajos> Trabajos { get; set; }
 }
 
 	
