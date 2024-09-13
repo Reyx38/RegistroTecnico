564e7ee9b2688531bc7763318f159d6195d91ec0
Reyphill Nuñez <reyphillduarte06@gmail.com>
2024-09-13 12:49:11 -0400

    Se agregaron validaciones a la hora de selecionar o agregar informacion

--- RegistroTecnicos/Models/Trabajos.cs
+++ RegistroTecnicos/Models/Trabajos.cs
@@ -8,7 +8,8 @@ public class Trabajos
 	[Key]
 	public int TrabajoId { get; set; }
 
-	[StringLength(250)]	
+	[StringLength(250)]
+	[Required (ErrorMessage ="Campo obligatario")]
 	public string? Descripcion { get;set; }
 
 	public DateTime? Fecha { get; set; } = DateTime.Now;
@@ -16,9 +17,11 @@ public class Trabajos
 	public double Monto { get; set; }
 
 	[ForeignKey("Tecnicos")]
+	[Required (ErrorMessage = "Debe de elegir un tecnico")]
 	public int TecnicoId { get; set; }
 	public Tecnicos? Tecnicos { get; set; }
 
+	[Required (ErrorMessage = "Debe de elegir un cliente")]
     public int? ClienteId { get; set; }
     public Clientes? Cliente { get; set; }
 }
