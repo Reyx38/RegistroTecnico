87c15e2fa4566008c0e0fc33faff2ef2c96f2ede
Reyphill Nuñez <reyphillduarte06@gmail.com>
2024-09-17 12:08:29 -0400

    Se agrego el modelo Prioridades

---
+++ RegistroTecnicos/Models/Prioridades.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class Prioridades
+{
+    [Key]
+    public int PrioridadId { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+    [RegularExpression("^[a-zA-ZÀ-ÿ\\s]+$", ErrorMessage = "Solo se permiten letras.")]
+    [StringLength(30)]
+    public string? Descripcion { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+    [Range(0.01, double.MaxValue, ErrorMessage = "El sueldo por hora debe ser mayor a 0.")]
+    [RegularExpression(@"^\d+$", ErrorMessage = "Solo se permiten números.")]
+    public int Tiempo { get; set; }
+}
