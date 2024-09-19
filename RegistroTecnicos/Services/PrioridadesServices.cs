7ad000924516ed90378dc817ab74c91a6c4cc99e
Reyphill Nuñez <reyphillduarte06@gmail.com>
2024-09-18 22:39:09 -0400

    Cambios en llamadas a la base de datos de tecnicos a prioridades

--- RegistroTecnicos/Services/PrioridadesServices.cs
+++ RegistroTecnicos/Services/PrioridadesServices.cs
@@ -7,56 +7,55 @@ namespace RegistroTecnicos.Services;
 
 public class PrioridadesServices(Contexto contexto)
 {
-    private readonly Contexto _contexto = contexto;
+	private readonly Contexto _contexto = contexto;
 
-    public async Task<bool> ExisteDescripcion(String descripcion)
-    {
-        return await _contexto.Prioridades.AnyAsync(p => p.Descripcion  == descripcion);
-    }
-    public async Task<bool> ExisteId(int id)
-    {
-        return await _contexto.Prioridades.AnyAsync(p => p.PrioridadId == id);
-    }
-    private async Task<bool> Insertar(Prioridades prioridad)
-    {
-        await _contexto.Prioridades.AddAsync(prioridad);
-        return await _contexto.SaveChangesAsync() > 0;
-    }
+	public async Task<bool> ExisteDescripcion(String descripcion)
+	{
+		return await _contexto.Prioridades.AnyAsync(p => p.Descripcion == descripcion);
+	}
+	public async Task<bool> ExisteId(int id)
+	{
+		return await _contexto.Prioridades.AnyAsync(p => p.PrioridadId == id);
+	}
+	private async Task<bool> Insertar(Prioridades prioridad)
+	{
+		await _contexto.Prioridades.AddAsync(prioridad);
+		return await _contexto.SaveChangesAsync() > 0;
+	}
 
-    private async Task<bool> Modificar(Prioridades prioridad)
-    {
-        _contexto.Update(prioridad);
-        return await _contexto.SaveChangesAsync() > 0;
-    }
+	private async Task<bool> Modificar(Prioridades prioridad)
+	{
+		_contexto.Update(prioridad);
+		return await _contexto.SaveChangesAsync() > 0;
+	}
 
-    public async Task<bool> Guardar(Prioridades prioridades)
-    {
-        if (!await ExisteId(prioridades.PrioridadId))
-            return await Insertar(prioridades);
+	public async Task<bool> Guardar(Prioridades prioridades)
+	{
+		if (!await ExisteId(prioridades.PrioridadId))
+			return await Insertar(prioridades);
 
-        return await Modificar(prioridades);
-    }
+		return await Modificar(prioridades);
+	}
 
-    public async Task<bool> Eliminar(int id)
-    {
-        return await _contexto.Prioridades.
-            Where(p => p.PrioridadId == id).ExecuteDeleteAsync() > 0;
-    }
+	public async Task<bool> Eliminar(int id)
+	{
+		return await _contexto.Prioridades.
+			Where(p => p.PrioridadId == id).ExecuteDeleteAsync() > 0;
+	}
 
-    public async Task<Tecnicos?> Buscar(int id)
-    {
-        return await _contexto.Tecnicos
-            .Include(t => t.TipoTecnico)
-            .FirstOrDefaultAsync(p => p.TecnicoId == id);
-    }
+	public async Task<Prioridades?> Buscar(int id)
+	{
+		return await _contexto.Prioridades
+			.FirstOrDefaultAsync(p => p.PrioridadId == id);
+	}
 
-    public async Task<List<Prioridades>> Listar(Expression<Func<Prioridades, bool>> criterio)
-    {
-        return await _contexto.Prioridades
-            .AsNoTracking()
-            .Where(criterio)
-            .ToListAsync();
-    }
+	public async Task<List<Prioridades>> Listar(Expression<Func<Prioridades, bool>> criterio)
+	{
+		return await _contexto.Prioridades
+			.AsNoTracking()
+			.Where(criterio)
+			.ToListAsync();
+	}
 
 
 }
