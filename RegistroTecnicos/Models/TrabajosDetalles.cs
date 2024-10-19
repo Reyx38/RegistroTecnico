cb081c6ecc3b26dd67d89e738964ff984deee135
Reyphill Nuñez <reyphillduarte06@gmail.com>
2024-10-19 14:50:04 -0400

    Creacion del modelo trabajosDetalles

---
+++ RegistroTecnicos/Models/TrabajosDetalles.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models
+{
+	public class TrabajosDetalles
+	{
+		[Key]
+		public int DetalleId { get; set; }
+		
+		[Required (ErrorMessage = "campo obligarior")]
+		public int TrabajoId { get; set; }
+
+		[Required(ErrorMessage = "campo obligarior")]
+		public int ArticuloId { get; set; }
+		
+		[Required(ErrorMessage = "campo obligarior")]
+		public int cantidad { get; set; }
+		
+		[Required(ErrorMessage = "campo obligarior")]
+		public double? costo { get; set; }
+		
+		[Required(ErrorMessage = "campo obligarior")]
+		public double? precio { get; set; }
+
+	}
+}
